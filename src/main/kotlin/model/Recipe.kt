9be068f0dc03360213fad91105763f03153be688
Mike McGee <mike@debian.mike.com>
2026-06-26 16:27:01 -0600

    recipe model

--- src/main/kotlin/model/Recipe.kt
+++ src/main/kotlin/model/Recipe.kt
@@ -1,8 +1,18 @@
 package com.model
 
+import kotlinx.serialization.Contextual
 import kotlinx.serialization.Serializable
+import java.time.LocalDateTime
 
 @Serializable
 data class Recipe(
     val id: Int,
+    val label: String,
+    val recipeCategory: String,
+    val ingredients: List<String>,
+    val steps: List<String>,
+    val notes: String,
+    val imageUrl: String,
+    val creatorId: String,
+    @Contextual val createdDate: LocalDateTime
 )
